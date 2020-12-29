db9e567a632b852b7de9df70cdbd497c5a953bf4
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2020-12-29 21:52:36 +0600

    maked game

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 const grid = document.querySelector(".grid");
 
+const gridItemsArray = document.querySelectorAll(".grid-item")
+
+let opennedAnimals = []
+
 grid.addEventListener("click",  (event) => {
-    event.target.classList.toggle("open")
-    console.log(event.target)
+    if(!event.target.classList.contains("grid")){
+        event.target.classList.toggle("open")
+        opennedAnimals.push(event.target)
+    if(event.target.classList.contains("open")){
+        event.target.classList.add(event.target.getAttribute("name"))
+
+    if(opennedAnimals[0].getAttribute("name") ===  opennedAnimals[1].getAttribute("name")){
+        console.log("same")
+    }else{
+        console.log("notsame")
+    }
+    }   
+    }
 })
+
+
+const animals = ["first", "second", "third", 'four', "five", 'six', "seven", "eight"]
+
+function shuffleAnimals () {
+    return animals.sort(() => Math.random() - 0.5)
+}
+shuffleAnimals()
+
+
+gridItemsArray.forEach((item, index) => {
+    item.setAttribute("name", animals[index])
+})
+
